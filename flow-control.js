495558b630eef1c1ee99eb33711544505f197f66
beloved-format-3447 <enc.mk.89@gmail.com>
2020-01-11 23:02:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,14 @@
 function basicTeenager(age) {
   if(age>=13&&age<=19){
-    return "You are a teenager!";
+    return "You are a teenager!"
   }
 }
 
 function teenager(age) {
   if(age>=13&&age<=19){
-    return "You are a teenanger!";
+    return "You are a teenager!"
   }else{
-    return "You are a teennager!";
+    return "You are a teenager!"
   }
 }
 
